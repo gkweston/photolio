cc6ac1ff95ca4f65f08d124ddaa02dc233079a91
gkweston <gkweston@gmail.com>
2021-09-27 09:46:31 -0500

    testing img placement

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,11 +4,22 @@ import ProjectContainer from '../ProjectContainer/ProjectContainer'
 import './Projects.css'
 
 const Projects = () => {
+  /* eslint-disable global-require */
+  const imgTest = require('../../Photos/imgTest.JPG')
+
   if (!projects.length) return null
 
   return (
     <section id='projects' className='section projects'>
+      
       <h2 className='section__title'>Projects</h2>
+
+      <img 
+        src={ imgTest.default }
+        height='200'
+        width='200'
+        alt='not found'
+      />
 
       <div className='projects__grid'>
         {projects.map((project) => (
@@ -17,6 +28,7 @@ const Projects = () => {
       </div>
     </section>
   )
+  /* eslint-enable global-require */
 }
 
 export default Projects
